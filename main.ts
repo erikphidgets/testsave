a480dc2e1f07e2d1a0167e8bf22d1024f3052a58
erikphidgets <112582242+erikphidgets@users.noreply.github.com>
2022-11-15 10:01:33 -0700

    added console logging for temp and hum

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+sensors.onTemperatureChange(Ports.P0, function (temperature) {
+    console.logValue("temp", temperature)
+})
+sensors.onHumidityChange(Ports.P0, function (humidity) {
+    console.logValue("hum", humidity)
+})
